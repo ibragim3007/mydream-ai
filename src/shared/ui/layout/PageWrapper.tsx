c576569d3f1955ee70bff355ad3f733a5054e457
Ibragim <lol.3007@mail.ru>
2025-03-29 22:07:44 -0700

    feat: gradient page viwe

--- src/shared/ui/layout/PageWrapper.tsx
+++ src/shared/ui/layout/PageWrapper.tsx
@@ -1,3 +1,4 @@
+import { LinearGradient } from 'expo-linear-gradient';
 import { useTheme } from '../../hooks/useTheme';
 import Grid, { GridProps } from '../grid/Grid';
 
@@ -5,5 +6,12 @@ type PageWrapper = GridProps;
 
 export default function PageWrapper({ ...props }: PageWrapper) {
   const colors = useTheme();
+
+  return (
+    <LinearGradient style={{ flex: 1 }} colors={['#052553', '#0E101D']}>
+      <Grid flex={1} {...props} />
+    </LinearGradient>
+  );
+
   return <Grid style={{ backgroundColor: props.color || colors.background.primary, flex: 1 }} {...props} />;
 }
