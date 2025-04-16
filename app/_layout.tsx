f20bbb2cdb82f263ca2f1a526292df43d48c57db
Ibragim <lol.3007@mail.ru>
2025-04-16 07:53:43 -0700

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -65,7 +65,7 @@ export default function RootLayout() {
         try {
           const appToken = await isAppToken();
 
-          const isUserOnboarded = false;
+          const isUserOnboarded = appToken;
 
           if (isUserOnboarded) {
             router.replace('/screens/homeScreen');
