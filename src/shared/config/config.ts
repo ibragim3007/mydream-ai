d0113eeba6040f90cc0493c00116e88a2c890ce0
Ibragim <lol.3007@mail.ru>
2025-04-25 20:19:31 -0700

    update vars

--- src/shared/config/config.ts
+++ src/shared/config/config.ts
@@ -1,7 +1,7 @@
 export const Environment = {
-  api_url: process.env.EXPO_PUBLIC_API_URL,
-  superwall_api_key: process.env.EXPO_PUBLIC_SUPERWALL_API_KEY,
-  revenue_api_key_IOS: process.env.EXPO_PUBLIC_REVENUE_API_KEY_IOS,
+  api_url: 'https://dream-lens.up.railway.app',
+  superwall_api_key: 'pk_23335537e4deb4fefc254bd5d7e5d3b0ff0df32966b22094',
+  revenue_api_key_IOS: 'appl_rNqmjVZghuWRbwfowlEHShPyhce',
   revenue_api_key_ANDROID: process.env.EXPO_PUBLIC_REVENUE_API_KEY_ANDROID,
-  vexo_api_key: process.env.EXPO_PUBLIC_VEXO_ANALYTICS_API_KEY,
+  vexo_api_key: '4cd8ffdf-b0fa-4d92-9f39-ab1ca521692f',
 };
