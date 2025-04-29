e069bab32aa342133f694e08685d0f6e3c4daa10
Ibragim <lol.3007@mail.ru>
2025-04-28 22:20:18 -0700

    add: de, es locales

--- src/shared/providers/i18n.ts
+++ src/shared/providers/i18n.ts
@@ -4,10 +4,12 @@ import { initReactI18next } from 'react-i18next';
 import en from '@/locales/en.json';
 import ru from '@/locales/ru.json';
 import es from '@/locales/es.json';
+import de from '@/locales/de.json';
+
 import { getDeviceLanguage } from '../helpers/getDeviceLanguage';
 
 i18n.use(initReactI18next).init({
-  resources: { en: { translation: en }, ru: { translation: ru }, es: { translation: es } },
+  resources: { en: { translation: en }, ru: { translation: ru }, es: { translation: es }, de: { translation: de } },
   lng: getDeviceLanguage(),
   fallbackLng: 'en',
   interpolation: { escapeValue: false },
